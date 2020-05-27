07fc4859f695d6a07072b23c70d8fe7c96c67d19
SkyWarZzeur <skywarzzeur@outlook.fr>
2020-05-27 12:46:15 +0200

    Fix alignment (fix #1)

--- views/elements/navbar.blade.php
+++ views/elements/navbar.blade.php
@@ -6,11 +6,11 @@
                     <img src="{{ site_logo() }}" alt="{{ site_name() }}" data-tilt data-tilt-scale="1.1">
                 </div>
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbar">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
                     @foreach($navbar as $element)
@@ -72,7 +72,7 @@
                             @if($server)
                                 @if(theme_config('use_play_button') !== 'on')
                                     <div data-toggle="tooltip" title="{{ trans('messages.actions.copy') }}" data-copy-target="address" data-copied-messages="{{ implode('|', trans('theme::prism.clipboard')) }}">
-                                        <input type="text" class="copy-address bg-primary-darker h5" id="address" style="width: {{ strlen($server->address) / 2 }}em" value="{{ $server->address }}" readonly>
+                                        <input type="text" class="copy-address bg-primary-darker h5 text-center" id="address" style="width: {{ strlen($server->address) / 2 }}em" value="{{ $server->address }}" readonly>
                                     </div>
                                 @else
                                     <div>
