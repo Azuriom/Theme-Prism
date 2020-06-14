75f5b71396202b8cabdcd291d0b78dfe20ddbbb6
MrMicky <git@mrmicky.fr>
2020-06-14 17:47:44 +0200

    Display background overlay only when title is not empty

--- views/home.blade.php
+++ views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', trans('messages.home'))
 
 @section('content')
-    <div class="home-background mb-4" style="background: url('{{ setting('background') ? image_url(setting('background')) : 'https://via.placeholder.com/2000x500' }}') no-repeat center / cover">
+    <div class="home-background @if(theme_config('title')) background-overlay @endif mb-4" style="background: url('{{ setting('background') ? image_url(setting('background')) : 'https://via.placeholder.com/2000x500' }}') no-repeat center / cover">
         @if(theme_config('title'))
             <div class="container h-100">
                 <div class="row align-items-center justify-content-center h-100">
