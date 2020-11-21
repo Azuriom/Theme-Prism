a4e9baf3247c103230b8ff9a1c25b3e5e0f3f0d2
SkyWarZzeur <skywarzzeur@outlook.fr>
2020-11-21 14:43:11 +0100

    Update

--- views/elements/navbar.blade.php
+++ views/elements/navbar.blade.php
@@ -16,7 +16,7 @@
                     @if($loop->index < ($loop->count / 2))
                         @if(!$element->isDropdown())
                             <li class="nav-item @if($element->isCurrent()) active @endif">
-                                <a class="nav-link" href="{{ $element->getLink() }}" @if($element->new_tab) target="_blank" rel="noopener" @endif>{{ $element->name }}</a>
+                                <a class="nav-link" href="{{ $element->getLink() }}" @if($element->new_tab) target="_blank" rel="noopener noreferrer" @endif>{{ $element->name }}</a>
                             </li>
                         @else
                             <li class="nav-item dropdown">
@@ -25,7 +25,7 @@
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     @foreach($element->elements as $childElement)
-                                        <a class="dropdown-item @if($childElement->isCurrent()) text-primary @endif" href="{{ $childElement->getLink() }}" @if($element->new_tab) target="_blank" rel="noopener" @endif>{{ $childElement->name }}</a>
+                                        <a class="dropdown-item @if($childElement->isCurrent()) text-primary @endif" href="{{ $childElement->getLink() }}" @if($element->new_tab) target="_blank" rel="noopener noreferrer" @endif>{{ $childElement->name }}</a>
                                     @endforeach
                                 </div>
                             </li>
@@ -40,7 +40,7 @@
                     @if($loop->index >= ($loop->count / 2))
                         @if(!$element->isDropdown())
                             <li class="nav-item @if($element->isCurrent()) active @endif">
-                                <a class="nav-link" href="{{ $element->getLink() }}" @if($element->new_tab) target="_blank" rel="noopener" @endif>{{ $element->name }}</a>
+                                <a class="nav-link" href="{{ $element->getLink() }}" @if($element->new_tab) target="_blank" rel="noopener noreferrer" @endif>{{ $element->name }}</a>
                             </li>
                         @else
                             <li class="nav-item dropdown">
@@ -49,7 +49,7 @@
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown{{ $element->id }}">
                                     @foreach($element->elements as $childElement)
-                                        <a class="dropdown-item @if($childElement->isCurrent()) active @endif" href="{{ $childElement->getLink() }}" @if($childElement->new_tab) target="_blank" rel="noopener" @endif>{{ $childElement->name }}</a>
+                                        <a class="dropdown-item @if($childElement->isCurrent()) active @endif" href="{{ $childElement->getLink() }}" @if($childElement->new_tab) target="_blank" rel="noopener noreferrer" @endif>{{ $childElement->name }}</a>
                                     @endforeach
                                 </div>
                             </li>
@@ -70,7 +70,7 @@
                     <div class="media-body">
                         @if($server && $server->isOnline())
                             @if(theme_config('use_play_button') !== 'on')
-                                <div data-toggle="tooltip" title="{{ trans('messages.actions.copy') }}" data-copy-target="address" data-copied-messages="{{ implode('|', trans('theme::prism.clipboard')) }}">
+                                <div data-toggle="tooltip" title="{{ trans('messages.actions.copy') }}" data-copy-target="address" data-copied-messages="{{ implode('|', is_array($t = trans('theme::prism.clipboard')) ? $t : []) }}">
                                     <input type="text" class="copy-address bg-primary-darker h5 text-center" id="address" style="width: {{ strlen($server->fullAddress()) / 2 }}em" value="{{ $server->fullAddress() }}" readonly aria-label="Address">
                                 </div>
                             @else
@@ -98,7 +98,7 @@
 
                     <span class="dropdown">
                         <a id="userDropdown" class="btn btn-outline-light btn-rounded dropdown-toggle my-1" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                            {{ Auth::user()->name }}
                         </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
